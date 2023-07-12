30d9f0f28ea8be7ca792cd94da7cbba25f15e894
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-12 01:21:14 -0700

    Adding services for "DbContext" and "Identity".

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Onyx.Models.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<OnyxDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+builder.Services
+    .AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<OnyxDBContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
