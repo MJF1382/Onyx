16fbee67080b3eb2352a02aca0d130901261d188
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-12 00:46:09 -0700

    Adding database entities.

---
+++ Models/Database/Entities/Entities.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Onyx.Models.Database.Entities
+{
+    public class Product
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+    }
+}
