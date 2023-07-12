e01f60a041c3be72e82f49bcea66f5ac5f3186c0
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-12 00:46:24 -0700

    Adding dbcontext for database.

---
+++ Models/Database/OnyxDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Onyx.Models.Database.Entities;
+
+namespace Onyx.Models.Database
+{
+    public class OnyxDBContext : DbContext
+    {
+        public OnyxDBContext()
+        {
+
+        }
+
+        public OnyxDBContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=(local);Database=OnyxDB;Trusted_Connection=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
