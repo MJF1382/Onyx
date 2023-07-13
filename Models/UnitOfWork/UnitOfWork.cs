e9f6bf16b8b7433249d36d0568822415a4440d0c
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-13 14:17:31 -0700

    Adding UnitOfWork class based on IUnitOfWork interface. Creating UnitOfWork class. Implementing IUnitOfWork methods.

---
+++ Models/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Onyx.Models.Database;
+using Onyx.Models.Repositories;
+
+namespace Onyx.Models.UnitOfWork
+{
+    public class UnitOfWork<TEntity> : IUnitOfWork<TEntity> where TEntity : class
+    {
+        private readonly OnyxDBContext _context;
+
+        public UnitOfWork(OnyxDBContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<TEntity> Repository => new Repository<TEntity>(_context);
+
+        public async Task<bool> Save()
+        {
+            return Convert.ToBoolean(_context.SaveChangesAsync());
+        }
+    }
+}
