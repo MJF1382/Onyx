e9911e5414aa826e916f17d54c9677c409b43ba4
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-13 14:03:42 -0700

    Fixing IRepository interface bugs.

--- Models/Repositories/IRepository.cs
+++ Models/Repositories/IRepository.cs
@@ -5,10 +5,10 @@ namespace Onyx.Models.Repositories
     public interface IRepository<TEntity>
     {
         Task<IEnumerable<TEntity>> GetAll();
-        Task<TEntity> FindById(object id);
+        Task<TEntity?> FindById(object id);
         Task<IEnumerable<TEntity>> FindByCondition(Expression<Func<TEntity, bool>> predicate);
-        void Add(TEntity entity);
-        void AddRange(IEnumerable<TEntity> entities);
+        Task Add(TEntity entity);
+        Task AddRange(IEnumerable<TEntity> entities);
         void Remove(TEntity entity);
         void RemoveRange(IEnumerable<TEntity> entities);
     }
