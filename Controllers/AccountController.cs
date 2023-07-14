055cfa4951e698decff413acc042fb2cb3d2318a
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-14 13:16:49 -0700

    Creating Account controller. Adding SignIn action result. Adding Register action result.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Onyx.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
