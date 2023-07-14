0cc6820967e8996d0f2ec005e12c0ee33f49dc24
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-14 13:23:44 -0700

    Adding ReturnUrl property.

--- Models/ViewModels/RegisterViewModel.cs
+++ Models/ViewModels/RegisterViewModel.cs
@@ -7,5 +7,6 @@
         public string? PhoneNumber { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
+        public string ReturnUrl { get; set; }
     }
 }
