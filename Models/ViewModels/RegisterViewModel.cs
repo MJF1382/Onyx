8f3af6668ddc4d422ba0437eeb7a436907c4a33f
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-16 02:52:52 -0700

    Changing nullable properties.

--- Models/ViewModels/RegisterViewModel.cs
+++ Models/ViewModels/RegisterViewModel.cs
@@ -2,9 +2,9 @@
 {
     public class RegisterViewModel
     {
-        public string? FullName { get; set; }
+        public string FullName { get; set; }
         public string? Email { get; set; }
-        public string? PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
         public string ReturnUrl { get; set; }
