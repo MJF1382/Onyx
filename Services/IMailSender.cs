57d1c92d9ff3ff3054dda7ed0713fd3ae5e0bfd7
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-17 00:53:38 -0700

    Adding MailSender service. Creating IMailSender interface. Implementing MailServer based on IMailSender

---
+++ Services/IMailSender.cs
@@ -0,0 +1,9 @@
+﻿using Onyx.Classes;
+
+namespace Onyx.Services
+{
+    public interface IMailSender
+    {
+        Task SendAsync(Email email);
+    }
+}
---
+++ Services/MailSender.cs
@@ -0,0 +1,34 @@
+﻿using Onyx.Classes;
+using System.Net;
+using System.Net.Mail;
+using Microsoft.Extensions.Options;
+
+namespace Onyx.Services
+{
+    public class MailSender : IMailSender
+    {
+        private readonly IOptions<MailSettings> _mailSettingsOptions;
+
+        public MailSender(IOptions<MailSettings> mailSettingsOptions)
+        {
+            _mailSettingsOptions = mailSettingsOptions;
+        }
+
+        public async Task SendAsync(Email email)
+        {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(_mailSettingsOptions.Value.SenderMail);
+            message.To.Add(email.To);
+            message.Subject = email.Subject;
+            message.Body = email.Body;
+            message.IsBodyHtml = true;
+
+            SmtpClient smtpClient = new SmtpClient(_mailSettingsOptions.Value.SmtpServer);
+            smtpClient.Port = 587;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.EnableSsl = true;
+            smtpClient.Credentials = new NetworkCredential(_mailSettingsOptions.Value.SenderMail, "Fathi2831Fathi");
+            await smtpClient.SendMailAsync(message);
+        }
+    }
+}
