8a91e48a5b5be44cd5af62ecefb4159bacdfce3f
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-17 00:55:26 -0700

    Adding ForgetPassword action result.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Onyx.Classes;
 using Onyx.Models.Identity.Entities;
 using Onyx.Models.ViewModels;
+using Onyx.Services;
 
 namespace Onyx.Controllers
 {
@@ -9,44 +11,16 @@ namespace Onyx.Controllers
     {
         private readonly SignInManager<AppUser> _signInManager;
         private readonly UserManager<AppUser> _userManager;
+        private readonly IMailSender _mailSender;
 
-        public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        public AccountController(
+            SignInManager<AppUser> signInManager,
+            UserManager<AppUser> userManager,
+            IMailSender mailSender)
         {
             _signInManager = signInManager;
             _userManager = userManager;
-        }
-
-        [HttpGet]
-        public IActionResult SignIn(string? returnUrl = null)
-        {
-            LoginViewModel viewModel = new LoginViewModel()
-            {
-                ReturnUrl = returnUrl ?? Url.Content("~/")
-            };
-
-            return View(viewModel);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> SignIn(LoginViewModel viewModel)
-        {
-            if (ModelState.IsValid)
-            {
-                AppUser user = await _userManager.FindByNameAsync(viewModel.UserName);
-
-                if (user != null)
-                {
-                    var result = await _signInManager.PasswordSignInAsync(user, viewModel.Password, viewModel.RememberMe, false);
-
-                    if (result.Succeeded)
-                    {
-                        return LocalRedirect(viewModel.ReturnUrl);
-                    }
-                }
-            }
-
-            return View(viewModel);
+            _mailSender = mailSender;
         }
 
         [HttpGet]
@@ -95,11 +69,78 @@ namespace Onyx.Controllers
         }
 
         [HttpGet]
+        public IActionResult SignIn(string? returnUrl = null)
+        {
+            LoginViewModel viewModel = new LoginViewModel()
+            {
+                ReturnUrl = returnUrl ?? Url.Content("~/")
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SignIn(LoginViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await _userManager.FindByNameAsync(viewModel.UserName);
+
+                if (user != null)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, viewModel.Password, viewModel.RememberMe, false);
+
+                    if (result.Succeeded)
+                    {
+                        return LocalRedirect(viewModel.ReturnUrl);
+                    }
+                }
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
         public async Task<IActionResult> SignOut()
         {
             await _signInManager.SignOutAsync();
 
             return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ForgetPassword(ForgetPasswordViewModel viewModel)
+        {
+            var user = await _userManager.FindByNameAsync(viewModel.UserName);
+
+            if (user != null)
+            {
+                string token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                string? callbackUrl = Url.Action("ResetPassword", "Account", new { userName = viewModel.UserName, token = token });
+
+                Email email = new Email()
+                {
+                    To = user.Email,
+                    Subject = "بازیابی رمز عبور",
+                    Body = "برای بازیابی رمز عبور روز لینک زیر کلیک کنید:" + "<br>" + "<a href='" + callbackUrl + "'>بازیابی</a>"
+                };
+
+                await _mailSender.SendAsync(email);
+            }
+            else
+            {
+                ModelState.AddModelError("", "کاربر مورد نظر یافت نشد.");
+            }
+
+            return View(viewModel);
         }
     }
 }
