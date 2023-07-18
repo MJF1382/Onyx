0cc5ae55de3a9b3021701889ede4bc92900f1062
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-18 13:10:48 -0700

    Adding ResetPasswordViewModel.

---
+++ Models/ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Onyx.Models.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Token { get; set; }
+    }
+}
