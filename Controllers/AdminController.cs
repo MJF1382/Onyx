9a17eebc43257b841acdb346f874e92698173f17
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-20 14:06:14 -0700

    Adding AdminController.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Onyx.Models.Identity.Entities;
+using Onyx.Models.ViewModels;
+
+namespace Onyx.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
