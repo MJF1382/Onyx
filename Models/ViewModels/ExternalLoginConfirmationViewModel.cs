a6d956a435953881aa31473023028dbd56deac6c
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-22 13:15:56 -0700

    Adding ExternalLoginConfirmationViewModel.

---
+++ Models/ViewModels/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Onyx.Models.ViewModels
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        public string FullName { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
