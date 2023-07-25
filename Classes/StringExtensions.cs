aaf6a1e060665d92d024038989350f63cd71b635
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-25 13:29:00 -0700

    Adding StringExtensions class.

---
+++ Classes/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Onyx.Classes
+{
+    public static class StringExtensions
+    {
+        public static bool HasValue(this string input)
+        {
+            if (string.IsNullOrEmpty(input) == false && string.IsNullOrWhiteSpace(input) == false)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
