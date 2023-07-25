3740ca1f95acf44cfee2cc707513610a747ea9aa
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-25 13:30:26 -0700

    Adding "Id" and "UserName" properties.

--- Models/ViewModels/UserViewModel.cs
+++ Models/ViewModels/UserViewModel.cs
@@ -2,7 +2,9 @@
 {
     public class UserViewModel
     {
+        public string Id { get; set; }
         public string FullName { get; set; }
+        public string UserName { get; set; }
         public string? Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
