f0afcf449206f748173e862edf5c23503fd87616
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-07-25 13:30:57 -0700

    Adding EditUserViewModel.

---
+++ Models/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Onyx.Models.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Password { get; set; }
+        public List<string> RolesName { get; set; }
+        public bool IsExternalLogin { get; set; }
+    }
+}
