1056c858e5ceb069e051034f0b2f0fa1d2bbf7e4
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-01 03:42:15 -0700

    Adding UserClaimsViewModel.

---
+++ Models/ViewModels/UserClaimsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Onyx.Classes;
+
+namespace Onyx.Models.ViewModels
+{
+    public class UserClaimsViewModel
+    {
+        public string UserId { get; set; }
+        public List<UserClaim> Claims { get; set; }
+    }
+}
