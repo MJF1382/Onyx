66832080c6a0ba7ca06e6889eddd6447a533f8ad
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-01 03:44:50 -0700

    Adding CreateClaimViewModel.

---
+++ Models/ViewModels/CreateClaimViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Onyx.Models.ViewModels
+{
+    public class CreateClaimViewModel
+    {
+        public string UserId { get; set; }
+        public string ClaimType { get; set; }
+        public string ClaimValue { get; set; }
+    }
+}
