8f5dbbffe6896a427c06fb321838ec3567b1430f
MJF1382 <seyedmohammadjavadfathi@gmail.com>
2023-08-01 03:45:31 -0700

    Adding EditClaimViewModel.

---
+++ Models/ViewModels/EditClaimViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Onyx.Models.ViewModels
+{
+    public class EditClaimViewModel
+    {
+        public string UserId { get; set; }
+        public string CurrentClaimType { get; set; }
+        public string ClaimType { get; set; }
+        public string ClaimValue { get; set; }
+    }
+}
